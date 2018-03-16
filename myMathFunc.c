e63c30497e7a1aae391d75e892719ba82f1d3301
Ibrahim Ijaz Sahi <35702562+ibrahim-sahi@users.noreply.github.com>
2018-03-17 00:30:21 +0500

    Add files via upload
    Lab#2 Question#1

---
+++ myMathFunc.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int isEqual(int x,int y){
+    if (x==y){
+    return 1;
+    }
+    else{
+    return -1;
+    }
+} 
+
+int swap (int *a, int *b){
+    *b=*a+*b;
+    *a=*b-*a;
+    *b=*b-*a;
+
+    
+    return 0;
+}

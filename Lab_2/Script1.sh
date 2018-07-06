65c07f2c25b637109e2fa9272c19b3469585c4d7
Ibrahim Ijaz Sahi <35702562+ibrahim-sahi@users.noreply.github.com>
2018-07-06 23:55:34 +0500

    Create Script1.sh

---
+++ Lab_2/Script1.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+if [ $# = 0 ]
+then
+	echo "None of the arguments were entered!"
+elif [ $# -gt 6 ]
+then
+	echo "More than 6 arguments were entered!"
+elif [ $# = 1 ]
+then
+	echo "Printing table"
+	declare -i num1
+	declare -i num2
+	num1=$1
+	num2=1
+	while [ $num2 -le 10 ]
+		do
+		ans=`expr $num1 \* $num2`
+		echo "$num1 * $num2 = $ans"
+		num2="$num2 + 1"
+	done
+elif [ $# = 3 ]
+then
+	echo "Printing table"
+	declare -i num1
+	declare -i num2
+	if [ $2 = '-s' ]
+	then
+	num1=$1
+	num2=$3	
+	while [ $num2 -le 10 ]
+		do
+		ans=`expr $num1 \* $num2`
+		echo "$num1 * $num2 = $ans"
+		num2="$num2 + 1"
+	done
+	elif [ $2 = '-e' ]
+	then
+	num1=$1
+	num2=1
+	while [ $num2 -le $3 ]
+		do
+		ans=`expr $num1 \* $num2`
+		echo "$num1 * $num2 = $ans"
+		num2="$num2 + 1"
+	done
+	else 
+		echo "Invalid argument/s!"
+	fi
+elif [ $# = 2 ]
+then
+	echo "Missing 3rd argument!"
+
+
+elif [ $# = 5 ]
+then
+	if [ $2 = '-s' -a $4 = '-e' ]
+	then
+	declare -i num1
+	declare -i num2
+	num1=$1
+	num2=$3
+	while [ $num2 -le $5 ]
+		do
+		ans=`expr $num1 \* $num2`
+		echo "$num1 * $num2 = $ans"
+		num2="$num2 + 1"
+	done
+	else
+		echo "Invalid argument/s"
+	fi
+elif [ $# = 6 ]
+then
+	if [ $2 = '-s' -a $4 = '-e' -a $6 = '-r' ]
+	then
+	declare -i num1
+	declare -i num2
+	num1=$1
+	num2=$5
+	while [ $num2 -ge $3 ]
+		do
+		ans=`expr $num1 \* $num2`
+		echo "$num1 * $num2 = $ans"
+		num2="$num2 - 1"
+	done
+	else
+		echo "Invalid argument/s"
+	fi
+fi
+
+
